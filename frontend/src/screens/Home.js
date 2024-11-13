5b6a583d355ca19d7db79325369fadffca16f09a
Aakrish Lama <al226na@student.lnu.se>
2024-11-13 22:51:40 +0100

    fetching global data into browser console

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Carousel from '../component/Carausel'
 import Navbar from '../component/Navbar'
 import Card from '../component/Card'
 
 
 export default function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+      let response = await fetch("http://localhost:9000/api/getFood", {
+          method: "POST",
+          headers: { "Content-Type": "multipart/form-data" }
+      });
+      response = await response.json();
+      console.log(response[0], response[1])
+      setFoodCat(response[0]);
+      setFoodItem(response[1]);
+  }
+
+  useEffect(() => {
+      loadData();
+  }, []);
   return (
     <div>
       <Navbar></Navbar>
